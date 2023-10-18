5049d35941f25c2e06329fbc2bd1cb546fbbcffd
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-10-18 09:24:04 +0530

    Create SalaryCalculator.java

---
+++ Java_DSA_23/functions/SalaryCalculator.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class SalaryCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input hourly wage and hours worked
+        System.out.print("Enter hourly wage: $");
+        double hourlyWage = scanner.nextDouble();
+
+        System.out.print("Enter hours worked in a month: ");
+        double hoursWorked = scanner.nextDouble();
+
+        // Calculate the monthly salary
+        double monthlySalary = hourlyWage * hoursWorked;
+
+        System.out.println("Your monthly salary is: $" + monthlySalary);
+
+        // Close the scanner
+        scanner.close();
+    }
+}
