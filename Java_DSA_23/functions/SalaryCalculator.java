8e09922f9daefc0e7ea045ad39eec81a51966d0e
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-10-18 10:38:20 +0530

    Add files via upload

--- Java_DSA_23/functions/SalaryCalculator.java
+++ Java_DSA_23/functions/SalaryCalculator.java
@@ -1,23 +1,41 @@
-//without function
 import java.util.Scanner;
 
 public class SalaryCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
-        // Input hourly wage and hours worked
-        System.out.print("Enter hourly wage: $");
-        double hourlyWage = scanner.nextDouble();
-
-        System.out.print("Enter hours worked in a month: ");
-        double hoursWorked = scanner.nextDouble();
-
-        // Calculate the monthly salary
-        double monthlySalary = hourlyWage * hoursWorked;
-
-        System.out.println("Your monthly salary is: $" + monthlySalary);
-
-        // Close the scanner
+        
+        System.out.print("Enter the basic salary: $");
+        double basicSalary = scanner.nextDouble();
+        
+        double hra = calculateHRA(basicSalary);
+        double da = calculateDA(basicSalary);
+        double ta = calculateTA(basicSalary);
+        double pf = 1400; // Fixed PF
+        
+        double netSalary = basicSalary + hra + da + ta - pf;
+        
+        System.out.println("Basic Salary: $" + basicSalary);
+        System.out.println("HRA: $" + hra);
+        System.out.println("DA: $" + da);
+        System.out.println("TA: $" + ta);
+        System.out.println("PF: $" + pf);
+        System.out.println("Net Salary: $" + netSalary);
+        
         scanner.close();
+    }
+    
+    // Function to calculate HRA (30% of basic salary)
+    public static double calculateHRA(double basicSalary) {
+        return 0.3 * basicSalary;
+    }
+    
+    // Function to calculate DA (20% of basic salary)
+    public static double calculateDA(double basicSalary) {
+        return 0.2 * basicSalary;
+    }
+    
+    // Function to calculate TA (10% of basic salary)
+    public static double calculateTA(double basicSalary) {
+        return 0.1 * basicSalary;
     }
 }
