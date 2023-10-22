4773a50a487b6707e0282896a674bc59bdd3c286
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-10-22 12:06:01 +0530

    Add files via upload

---
+++ Java_DSA_23/functions/InsuranceCompany.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class InsuranceCompany {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter your Name: ");
+        String inputString = sc.nextLine();
+         System.out.print("Enter your Gender: ");
+        String genderString = sc.nextLine();
+         System.out.print("Enter your Gender: ");
+        int age = sc.nextInt();
+  
+
+    }
+    
+}
