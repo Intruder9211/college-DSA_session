e86459e7d0bc7bb8e0b4cd1c4dd199198db6ed75
Mohit Singh <123624325+Intruder9211@users.noreply.github.com>
2023-10-25 14:45:32 +0530

    Add files via upload

---
+++ Java_DSA_23/functions/PerfectNumberChecker.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PerfectNumberChecker {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+
+        if (isPerfectNumber(number)) {
+            System.out.println(number + " is a perfect number.");
+        } else {
+            System.out.println(number + " is not a perfect number.");
+        }
+
+        input.close();
+    }
+
+    static boolean isPerfectNumber(int num) {
+        if (num <= 0) {
+            return false; // Perfect numbers are defined for positive integers.
+        }
+
+        int sumOfDivisors = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) {
+                sumOfDivisors += i;
+            }
+        }
+
+        return sumOfDivisors == num;
+    }
+}
+
